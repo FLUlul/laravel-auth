69f69d7cf1d192e58424d9089f1aaf08428971ef
FLU <valerio.massj@gmail.com>
2022-02-04 10:42:12 +0100

    update: add error catch, add guest section

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -14,6 +14,15 @@
         @include('components.header')
 
         <div class="content">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             @yield('content')
         </div>
         
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,42 +7,45 @@
     <h1>Login or register to see the infos.</h1>
     @endauth
 
-    <h3>Login:</h3>
-    <form method="POST" action="{{route('login')}}">
+    @guest
+        
+        <h3>Login:</h3>
+        <form method="POST" action="{{route('login')}}">
 
-        @method("POST")
-        @csrf
+            @method("POST")
+            @csrf
 
-        <label for="email">Email: </label>
-        <input type="email" name="email" placeholder="email">
+            <label for="email">Email: </label>
+            <input type="email" name="email" placeholder="email">
 
-        <label for="password">Password: </label>
-        <input type="password" name="password" placeholder="password">
+            <label for="password">Password: </label>
+            <input type="password" name="password" placeholder="password">
 
-        <button type="submit">Login</button>
+            <button type="submit">Login</button>
 
-    </form>
-    <br><hr><hr><br>
-    
-    <h3>Register:</h3>
-    <form method="POST" action="{{route('register')}}">
+        </form>
+        <br><hr><hr><br>
+        
+        <h3>Register:</h3>
+        <form method="POST" action="{{route('register')}}">
 
-        @method("POST")
-        @csrf
+            @method("POST")
+            @csrf
 
-        <label for="name">Name: </label>
-        <input type="text" name="name" placeholder="name">
+            <label for="name">Name: </label>
+            <input type="text" name="name" placeholder="name">
 
-        <label for="email">Email: </label>
-        <input type="email" name="email" placeholder="email">
+            <label for="email">Email: </label>
+            <input type="email" name="email" placeholder="email">
 
-        <label for="password">Password: </label>
-        <input type="password" name="password" placeholder="password">
+            <label for="password">Password: </label>
+            <input type="password" name="password" placeholder="password">
 
-        <label for="password-confirm">Password Confirm: </label>
-        <input type="password" name="password_confirmation" placeholder="password-confirm">
+            <label for="password-confirm">Password Confirm: </label>
+            <input type="password" name="password_confirmation" placeholder="password-confirm">
 
-        <button type="submit">Register</button>
+            <button type="submit">Register</button>
 
-    </form>
+        </form>
+    @endguest
 @endsection
